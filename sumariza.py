088b11e5a2a19045d01225ca3e52ed2c05b238df
Thiago marques <48632838+MarquesThiago@users.noreply.github.com>
2023-03-02 17:20:32 -0300

    added configuretions and change files from flollow petterns, with PEP8 and correct erros in english

--- sumariza.py
+++ sumariza.py
@@ -1,41 +1,50 @@
-import re
-import string
 import nltk
-import pandas as pd
-import numpy as np
-from luhn import (sumarize_by_luhn, variables)
-from  common import (simple_clear, stop_word, wiegth_sentency, normalize_text, frenquency, frequency_in_text, init)
+from common import (
+    simple_clear,
+    weight_sentence,
+    normalize_text,
+    init,
+    frequency
+)
+
 
 init()
 
-def resume(sentency, list_):
-    
+
+def resume(sentence, list_):
+
     '''
-    resume a text starting of the dictonary with grades and index of the sentences
-    :param sentency: dict -> sentences and weight
+    resume a text starting of the dictionary with grades and index of the
+    sentences
+    :param sentence: dict -> sentences and weight
     :param list_weight:list -> list of the index of the sent 
     :return string  " ".join(sent):
-    
+
     '''
-    sent = [sentency[index] for index in list_]
+    sent = [sentence[index] for index in list_]
     return " ".join(sent)
 
-def sumarize_per_frequency(text, n = 10, order = False, lemma = False):
-    
+
+def sumarize_per_frequency(text, n=10, order=False, lemma=False):
+
     harmonized_words = normalize_text(text, lemma)
     harmonized_text = " ".join(harmonized_words)
-    
-    frenquencia = frenquency(harmonized_words, harmonized_text)
-    max_frenquency = max(frenquencia.values())
-    wiegth = { word : note/max_frenquency for word, note in frenquencia.items()}
-    sentenc_text = nltk.sent_tokenize(simple_clear(text), language = "Portuguese")
-        
-    wiegth_sent = [[index , wiegth_sentency(sentenc_text[index], wiegth)] for index in range(len(sentenc_text))]
-    ordenate = sorted(wiegth_sent, key = lambda x: x[1] , reverse = True)
-    
-    ordenate_rank = [index[0] for index in ordenate]
+
+    frequency_word = frequency(harmonized_words, harmonized_text)
+    max_frequency = max(frequency_word.values())
+
+    weight = {word: note/max_frequency 
+              for word, note in frequency_word.items()}
+
+    sentence_text = nltk.sent_tokenize(
+        simple_clear(text), language="Portuguese")
+
+    weight_sent = [[index, weight_sentence(sentence_text[index], weight)]
+                   for index in range(len(sentence_text))]
+    ordinate = sorted(weight_sent, key=lambda x: x[1], reverse=True)
+
+    ordinate_rank = [index[0] for index in ordinate]
     if order:
-        ordenate_rank = sorted(ordenate_rank[:n])
+        ordinate_rank = sorted(ordinate_rank[:n])
 
-    return resume(sentenc_text, ordenate_rank[:n])
-
+    return resume(sentence_text, ordinate_rank[:n])
